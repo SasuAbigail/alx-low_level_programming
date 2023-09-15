fb166d44225e41686880b090b3aa28888b082f43
SasuAbigail <sasuabigail234@gmail.com>
2023-09-15 02:37:24 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,13 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
